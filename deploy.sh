02989ca4f650db6561a22091d387bae6e3eeddcc
Jérôme Alves <j.alves@me.com>
2018-01-14 11:39:23 +0100

    Remove minimo theme

--- deploy.sh
+++ deploy.sh
@@ -5,25 +5,20 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
-
-# Commit changes.
+# Commit message.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
-  then msg="$1"
+then msg="$1"
 fi
-git commit -m "$msg"
 
-# Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
-cd ..
-
+# Commit and Push public submodule
+cd public
 git add .
 git commit -m "$msg"
 git push origin master
 
+# Commit and Push project root
+cd ..
+git add .
+git commit -m "$msg"
+git push origin master
